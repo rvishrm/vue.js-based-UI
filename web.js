55614c4e74480a8185e36120c330736c5967d021
ravi <rvishrma007@gmail.com>
2020-08-26 00:58:26 +0530

    responsive

--- web.js
+++ web.js
@@ -15,23 +15,23 @@ new Vue({
     methods: {
         changePicRight: function(){
 
-            this.count++;
            
         //    console.log(this.$refs.one.style);
             // console.log(this.pictureNumber);
-            this.xyy += 12.5;
+            this.xyy += 12.62;
             this.url = `transform: translateX(`+ -this.xyy+`%); transition:transform 0.5s ease-in-out;`;
             
+            this.count++;
             console.log(this.url);
             // console.log(typeof String(this.pictureNumber+1))
         },
         changePicLeft: function(){
-            this.count--;
+           
            
             // console.log(this.pictureNumber);
-            this.xyy -=12.5;
+            this.xyy -=12.62;
             this.url =`transform: translateX(`+ -this.xyy+`%); transition:transform 0.5s ease-in-out;`;
-            
+            this.count--;
             console.log(this.url);
             // console.log(typeof String(this.pictureNumber+1))
         },
